e1348ce91372efe4b048a6a33d7e76cb25ae9977
Patrick Tran <patrick.tran06@gmail.com>
2024-02-28 08:13:35 -0800

    Superuser guard (#6)
    * guard superuser on download
    
    * delete post in index
    
    * (lint) make changes
    
    * (lint) add error logs

--- word_steno/clips/views.py
+++ word_steno/clips/views.py
@@ -1,6 +1,8 @@
 import json
+import logging
 import math
 
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.postgres.search import SearchQuery
 from django.contrib.postgres.search import SearchRank
 from django.contrib.postgres.search import SearchVector
@@ -24,160 +26,90 @@ from .utils import transcribe_audio
 # Static
 TWO_HOURS = 2 * 60 * 60
 
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 def index(request):
-    # Handle the POST request
-    if request.method == "POST":
-        try:
-            video_url = request.POST.get("youtube_url")
+    search_term = request.GET.get("query", "")
+    if not search_term:
+        return render(request, "clips/search.html")
 
-            if not video_url:
-                return HttpResponse("No URL provided", status=400)
+    # Define the search query
+    search_query = SearchQuery(search_term, config="english")
 
-            print("Start")
-
-            # Initialize video url
-            yt = YouTube(video_url)
-            if yt.length > TWO_HOURS:
-                return HttpResponse(
-                    "Youtube Video is greater than 2 hours. Please use a shorter video.",
-                    status=400,
-                )
-            print("checked length")
-
-            # Check if the Clip with this video_id already exists
-            video_id = extract_youtube_video_id(video_url)
-            print("Extract Video ID", video_id)
-            clip, created = Clip.objects.get_or_create(
-                video_id=video_id,
-                defaults={
-                    "url": video_url,
-                    "title": yt.title,
-                    "length": yt.length,
-                    "channel_title": yt.author,
-                    "description": get_description(video_url),
-                    "published_at": yt.publish_date,
-                },
-            )
-
-            if not created:
-                print("Redirect clip to", clip.id)
-                return redirect(reverse("clips:clip", args=[clip.id]))
-
-            print("Save Database ", clip.id)
-
-            # Download & Upload Audio to S3
-            video_details = download_audio(yt)
-            print("Video Detail", video_details)
-
-            # Transcribe audio file from S3
-            transcribed_audio = transcribe_audio(video_details["object_name"])
-
-            # modify transcribed data to json format
-            data = transcribed_audio.to_json()
-            data = json.loads(data)
-            print("Transcibed Audio", data)
-            deepgram_object = data["results"]["channels"][0]["alternatives"][0]
-            paragraphs_data = deepgram_object["paragraphs"]["paragraphs"]
-
-            # Save transcribed data to clip model
-            clip.storage_path = video_details["storage_path"]
-            clip.full_transcription = deepgram_object["paragraphs"]["transcript"]
-            clip.summary = data["results"]["summary"]["short"]
-            clip.paragraphs = paragraphs_data
-            clip.words = deepgram_object["words"]
-
-            clip.save()
-            print("Database Update")
-
-            # Save Paragraphs in ClipParagraph model
-            extract_paragraphs(paragraphs_data, clip)
-            print("Paragraphs Update")
-
-            # Redirect to results page
-            return redirect(reverse("clips:clip", args=[clip.id]))
-        except Exception as e:
-            return HttpResponse(f"An error occurred: {e}", status=500)
-
-    # Handle the GET request
-    else:
-        search_term = request.GET.get("query", "")
-        if search_term:
-            # Define the search query
-            search_query = SearchQuery(search_term, config="english")
-
-            # Annotate each ClipParagraph with a search vector and rank based on the 'text' key of the sentences JSONB field
-            clip_paragraphs = (
-                ClipParagraph.objects.annotate(
-                    # search_vector=SearchVector(RawSQL("jsonb_path_query_array(sentences, '$[*].text')::text", []), config='english')
-                    search_vector=SearchVector(
-                        "full_transcription",
-                        config="english",
-                        weight="A",
-                    ),
-                )
-                .annotate(
-                    rank=SearchRank(F("search_vector"), search_query),
-                )
-                .filter(search_vector=search_query)
-                .order_by("-rank")
-                .select_related("clip")
-            )
-
-            grouped_results = {}
-            for cp in clip_paragraphs:
-                clip_id = cp.clip.id
-                if clip_id not in grouped_results:
-                    grouped_results[clip_id] = {
-                        "clip_paragraph_id": cp.id,
-                        "clip_id": cp.clip.id,
-                        "clip_url": f"{cp.clip.url}&t={int(cp.start-3)}",  # Example of including Clip detail
-                        "clip_title": cp.clip.title,  # Example of including Clip detail
-                        "sentences": cp.sentences,
-                        "full_transcription": [],
-                        "rank": cp.rank,
-                        "start": math.floor(
-                            cp.start,
-                        ),  # Example of including Clip detail, cp.start,
-                        "end": math.ceil(cp.end),
-                        "speaker": cp.speaker,
-                        "channel_title": cp.clip.channel_title,
-                        "video_length": cp.clip.length,
-                        "video_transcription": cp.clip.full_transcription,
-                        "video_summary": cp.clip.summary,
-                        "video_id": cp.clip.video_id,
-                        "embed_url": f"https://www.youtube.com/embed/{extract_youtube_video_id(cp.clip.url)}?start={math.floor(cp.start)}&end={math.ceil(cp.end)}",
-                    }
-                # Now append the full_transcription and any other necessary details
-                grouped_results[clip_id]["full_transcription"].append(
-                    {
-                        "text": cp.full_transcription,
-                        "start": math.floor(cp.start),
-                        "end": math.ceil(cp.end),
-                    },
-                )
-                # Repeat for other fields as necessary
-
-            # After appending all full_transcription entries to grouped_results
-            for clip_id, group in grouped_results.items():
-                # Sort the full_transcription list for each group by the 'start' value
-                group["full_transcription"] = sorted(
-                    group["full_transcription"],
-                    key=lambda x: x["start"],
-                )
-
-            # Convert grouped_results to a list format as per your requirement
-            results = [value for key, value in grouped_results.items()]
-
-        else:
-            results = Clip.objects.none()
-            return render(request, "clips/search.html")
-        return render(
-            request,
-            "clips/search.html",
-            {"results": results, "search_term": search_term},
+    # Annotate each ClipParagraph with a search vector and rank based on the 'text'
+    # key of the sentences JSONB field
+    clip_paragraphs = (
+        ClipParagraph.objects.annotate(
+            search_vector=SearchVector(
+                "full_transcription",
+                config="english",
+                weight="A",
+            ),
         )
+        .annotate(
+            rank=SearchRank(F("search_vector"), search_query),
+        )
+        .filter(search_vector=search_query)
+        .order_by("-rank")
+        .select_related("clip")
+    )
+
+    grouped_results = {}
+    for cp in clip_paragraphs:
+        clip_id = cp.clip.id
+        if clip_id not in grouped_results:
+            grouped_results[clip_id] = {
+                "clip_paragraph_id": cp.id,
+                "clip_id": cp.clip.id,
+                "clip_url": f"{cp.clip.url}&t={int(cp.start-3)}",
+                "clip_title": cp.clip.title,
+                "sentences": cp.sentences,
+                "full_transcription": [],
+                "rank": cp.rank,
+                "start": math.floor(
+                    cp.start,
+                ),
+                "end": math.ceil(cp.end),
+                "speaker": cp.speaker,
+                "channel_title": cp.clip.channel_title,
+                "video_length": cp.clip.length,
+                "video_transcription": cp.clip.full_transcription,
+                "video_summary": cp.clip.summary,
+                "video_id": cp.clip.video_id,
+                "embed_url": (
+                    f"https://www.youtube.com/embed/{extract_youtube_video_id(cp.clip.url)}"
+                    f"?start={math.floor(cp.start)}"
+                    f"&end={math.ceil(cp.end)}"
+                ),
+            }
+        # Now append the full_transcription and any other necessary details
+        grouped_results[clip_id]["full_transcription"].append(
+            {
+                "text": cp.full_transcription,
+                "start": math.floor(cp.start),
+                "end": math.ceil(cp.end),
+            },
+        )
+        # Repeat for other fields as necessary
+
+    # After appending all full_transcription entries to grouped_results
+    for group in grouped_results.values():
+        # Sort the full_transcription list for each group by the 'start' value
+        group["full_transcription"] = sorted(
+            group["full_transcription"],
+            key=lambda x: x["start"],
+        )
+
+    # Convert grouped_results to a list format as per your requirement
+    results = [value for key, value in grouped_results.items()]
+
+    return render(
+        request,
+        "clips/search.html",
+        {"results": results, "search_term": search_term},
+    )
 
 
 def clip(request, clip_id, start=0):
@@ -193,10 +125,9 @@ def clip(request, clip_id, start=0):
                     clip_id=clip_id,
                     speaker=old_speaker,
                 ).update(speaker=updated_speaker)
-                # print(ClipParagraph.objects.filter(clip_id=clip_id, speaker=old_speaker).first())
                 return redirect(
                     reverse("clips:clip", args=[clip_id]),
-                )  # Redirect to a new URL to prevent form resubmission
+                )
 
         # fetch clip data
         clip = Clip.objects.get(id=clip_id)
@@ -240,19 +171,21 @@ def clip(request, clip_id, start=0):
 
 
 def update_speaker(request, clip_id):
-    if request.method == "POST":
-        # Extract the updated speaker information from the POST data
-        paragraph_id = request.POST.get("paragraph_id")
-        updated_speaker = request.POST.get("new_speaker")
+    # Extract the updated speaker information from the POST data
+    paragraph_id = request.POST.get("paragraph_id")
+    updated_speaker = request.POST.get("new_speaker")
 
-        # Update ClipParagraph instances
-        if updated_speaker is not None:
-            ClipParagraph.objects.filter(clip_id=clip_id, id=paragraph_id).update(
-                speaker=updated_speaker,
-            )
-            return redirect(
-                reverse("clips:clip", args=[clip_id]),
-            )  # Redirect to a new URL to prevent form resubmission
+    # Update ClipParagraph instances
+    if updated_speaker is not None:
+        ClipParagraph.objects.filter(clip_id=clip_id, id=paragraph_id).update(
+            speaker=updated_speaker,
+        )
+        # Redirect to a new URL to prevent form resubmission
+        return redirect(
+            reverse("clips:clip", args=[clip_id]),
+        )
+
+    return HttpResponse("No speaker name provided.", status=400)
 
 
 def channels(request):
@@ -283,12 +216,76 @@ def channels(request):
         return HttpResponse("Channels do not exist.", status=500)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def download(request):
-    try:
-        return render(request, "clips/download_form.html")
+    if request.method == "POST":
+        try:
+            video_url = request.POST.get("youtube_url")
 
-    except Clip.DoesNotExist:
-        return HttpResponse("Channels do not exist.", status=500)
+            if not video_url:
+                return HttpResponse("No URL provided", status=400)
+
+            # Initialize video url
+            yt = YouTube(video_url)
+            if yt.length > TWO_HOURS:
+                return HttpResponse(
+                    (
+                        "Youtube Video is greater than 2 hours. "
+                        "Please use a shorter video."
+                    ),
+                    status=400,
+                )
+
+            # Check if the Clip with this video_id already exists
+            video_id = extract_youtube_video_id(video_url)
+            clip, created = Clip.objects.get_or_create(
+                video_id=video_id,
+                defaults={
+                    "url": video_url,
+                    "title": yt.title,
+                    "length": yt.length,
+                    "channel_title": yt.author,
+                    "description": get_description(video_url),
+                    "published_at": yt.publish_date,
+                },
+            )
+
+            if not created:
+                return redirect(reverse("clips:clip", args=[clip.id]))
+
+            # Download & Upload Audio to S3
+            video_details = download_audio(yt)
+
+            # Transcribe audio file from S3
+            transcribed_audio = transcribe_audio(video_details["object_name"])
+
+            # modify transcribed data to json format
+            data = transcribed_audio.to_json()
+            data = json.loads(data)
+            deepgram_object = data["results"]["channels"][0]["alternatives"][0]
+            paragraphs_data = deepgram_object["paragraphs"]["paragraphs"]
+
+            # Save transcribed data to clip model
+            clip.storage_path = video_details["storage_path"]
+            clip.full_transcription = deepgram_object["paragraphs"]["transcript"]
+            clip.summary = data["results"]["summary"]["short"]
+            clip.paragraphs = paragraphs_data
+            clip.words = deepgram_object["words"]
+
+            clip.save()
+
+            # Save Paragraphs in ClipParagraph model
+            extract_paragraphs(paragraphs_data, clip)
+
+            # Redirect to results page
+            return redirect(reverse("clips:clip", args=[clip.id]))
+        except Exception as e:
+            logging.exception(
+                "Exception: download POST: downloading, transcribing and saving the clip.",
+            )
+            return HttpResponse(f"An error occurred: {e}", status=500)
+    else:
+        return render(request, "clips/download_form.html")
 
 
 def delete(request, clip_id):
@@ -310,14 +307,12 @@ def paragraph(request, clip_id):
         paragraphs_data = clip.paragraphs
 
         if paragraphs_data:
-            # ClipParagraph.objects.filter(clip=clip).delete()  # Remove existing paragraphs to prevent duplicates
             for paragraph in paragraphs_data:
                 # Create Full Transcription for the paragraph
                 full_transcription = ""
                 sentences = paragraph.get("sentences")
                 if sentences:
                     for sentence in sentences:
-                        print(sentence.get("text"), sentence)
                         full_transcription += sentence.get("text") + " "
 
                 ClipParagraph.objects.create(
@@ -336,22 +331,14 @@ def paragraph(request, clip_id):
 def embedding_save(request):
     try:
         for clip in Clip.objects.all():
-            print(extract_youtube_video_id(clip.url))
             clip.video_id = extract_youtube_video_id(clip.url)
             clip.save()
 
         return HttpResponse("Embedding successfully.", status=200)
 
     except Exception as e:
+        logging.exception("Exception: embedding_save.")
         return HttpResponse(f"An error occurred in embedding: {e}", status=500)
-
-    # try:
-    #     transcriptions = ClipParagraph.objects.all()
-    #     print(transcriptions)
-    #     embed_transcriptions(transcriptions)
-    #     return HttpResponse("Embedding successfully.", status=200)
-    # except Exception as e:
-    #     return HttpResponse(f"An error occurred in embedding: {e}", status=500)
 
 
 def embedding(request):
@@ -362,8 +349,8 @@ def embedding(request):
             {
                 "clip_paragraph_id": cp.id,
                 "clip_id": cp.clip.id,
-                "clip_url": f"{cp.clip.url}&t={int(cp.start-3)}",  # Example of including Clip detail
-                "clip_title": cp.clip.title,  # Example of including Clip detail
+                "clip_url": f"{cp.clip.url}&t={int(cp.start-3)}",
+                "clip_title": cp.clip.title,
                 "sentences": cp.sentences,
                 "full_transcription": cp.full_transcription,
                 "start": cp.start,
